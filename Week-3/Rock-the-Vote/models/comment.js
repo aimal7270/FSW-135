9fce0b67626adedbdd8b17a0fd287e868cf11ec6
Aimal Khan <Aimal.khan@bryanuniversity.edu>
2021-08-23 23:55:02 -0400

    week-3

---
+++ Week-3/Rock-the-Vote/models/comment.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema({
+    
+newComment: {
+    type: String,
+    required: true
+},
+user:{
+    type:Schema.Types.ObjectId,
+    ref:"User",
+    required: ture
+},
+
+})
+
+module.exports = mongoose.model( "Comment", commentSchema )
