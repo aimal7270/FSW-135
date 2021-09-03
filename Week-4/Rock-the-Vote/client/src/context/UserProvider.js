6387196b75591b68fff8591977a71fb1770e937e
Aimal Khan <Aimal.khan@bryanuniversity.edu>
2021-09-02 23:41:57 -0400

    gg

--- Week-4/Rock-the-Vote/client/src/context/UserProvider.js
+++ Week-4/Rock-the-Vote/client/src/context/UserProvider.js
@@ -18,7 +18,7 @@ export default function UserProvider(props) {
     userComments: [],
     issueComments: [],
 
-    function signup(credentials) {
+    function :signup(credentials) {
         axios
           .post("/auth/signup", credentials)
           .then((res) => {
